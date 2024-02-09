07ee7fb0eec9339d14ebcad8ac333f153455d0c4
mohit <mohitsharma55337@gmail.com>
2024-02-09 12:01:47 +0530

    test

--- next.config.js
+++ next.config.js
@@ -1,18 +1,28 @@
 module.exports = {
-  webpack: (config) => {
-    config.module.rules.push({
-      test: /\.(mp4|webm)$/,
-      use: {
-        loader: "file-loader",
-        options: {
-          publicPath: "/",
-          outputPath: "static/videos/",
-          name: "[name].[ext]",
-          esModule: true,
+    webpack: (config) => {
+      config.module.rules.push({
+        test: /\.(mp4|webm)$/,
+        use: {
+          loader: 'file-loader',
+          options: {
+            publicPath: '/',
+            outputPath: 'static/videos/',
+            name: '[name].[ext]',
+            esModule: true,
+          },
         },
-      },
-    });
-
-    return config;
-  },
-};
+      });
+  
+      return config;
+    },
+    async redirects() {
+      return [
+        {
+          source: '/',
+          destination: '/homepage',
+          permanent: true,
+        },
+      ]
+    },
+  };
+  
