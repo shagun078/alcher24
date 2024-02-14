71dd5505d972fa62658fb5ff2fc32490772d7bb2
TheAdich <adidadhich2003@gmail.com>
2024-02-15 00:09:40 +0530

    silly_change

--- src/app/home/home.js
+++ src/app/home/home.js
@@ -663,10 +663,10 @@ export default function Home() {
             <OrbitControls
               ref={ref}
               target={[-3.5, 1.4, -1.8]}
-              enableZoom={true}
+              enableZoom={false}
               enableDamping={true}
-             // minPolarAngle={Math.PI / 3}
-              //maxPolarAngle={Math.PI / 2}
+              minPolarAngle={Math.PI / 3}
+              maxPolarAngle={Math.PI / 2}
               dampingFactor={0.02}
               enabled={true}
             />
