fc640256f2a42ae784e0ddebcc055bf8bc5fd953
TheAdich <adidadhich2003@gmail.com>
2024-02-15 19:24:50 +0530

    events_update

--- src/app/home/components/Animations.js
+++ src/app/home/components/Animations.js
@@ -5,7 +5,7 @@ const Animations = () => {
     
     return (
         <div className={styles.animes}>
-            <div className={styles.first}><div>27 EDITIONS</div></div>
+            <div className={styles.first}><div>28 EDITIONS</div></div>
             <div className={styles.second}><div>140K FOOTFALL</div></div>
             <div className={styles.third}><div>100+ EVENTS</div></div>
             <div className={styles.fourth}><div>500+ COLLEGES</div></div>
