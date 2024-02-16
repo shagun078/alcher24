6a2deddbaddfde75efb0b0e067eb9f8c9bccc1f3
skrj_ <shubhkarjha533@gmail.com>
2024-02-16 21:41:07 +0530

    star,optim

--- src/app/kartavya/kartavya.js
+++ src/app/kartavya/kartavya.js
@@ -106,10 +106,11 @@ export default function Kartavya() {
                     width: "5vw",
                     height: "auto",
                     float: "left",
+                    backgroundRepeat: "no-repeat",
                   }}
                   quality={100}
                   placeholder="blur"
-                  className="star2"
+                  className="star2_k"
                 />
               </div>
             </div>
