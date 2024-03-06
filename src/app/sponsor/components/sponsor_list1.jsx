b85a8ded5486b9366a16728070fd3b8f40a16221
skrj_ <shubhkarjha533@gmail.com>
2024-03-06 15:35:51 +0530

    moved

--- src/app/sponsor/components/sponsor_list1.jsx
+++ src/app/sponsor/components/sponsor_list1.jsx
@@ -3,6 +3,13 @@
 function getCards(){
     const data = [
       {
+        "id": "18",
+        "name": "Outlook",
+        "partner": "Digital News Partner",
+        "img": "/sponsor_res/digital_news/Outlook_Logo.png",
+        "slink": "https://www.outlookindia.com/"
+      },
+      {
         "id": "1",
         "name": "Trending on Guwahati",
         "partner": "Digital Media Partner",
@@ -120,13 +127,6 @@ function getCards(){
         "partner": "Digital News Partner",
         "img": "/sponsor_res/digital_news/siliconindia.png",
         "slink": "https://www.siliconindia.com/"
-      },
-      {
-        "id": "18",
-        "name": "Outlook",
-        "partner": "Digital News Partner",
-        "img": "/sponsor_res/digital_news/Outlook_Logo.png",
-        "slink": "https://www.outlookindia.com/"
       }
     ]
     return data;
